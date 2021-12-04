62066df6a29dc328cd21ccde70b905681408ae5f
Shahad Suman <shahad.sumann@gmail.com>
2021-12-04 15:09:37 +0545

    Moved repo to git

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def contents() -> 'html':
+    return render_template('contents.html')
+@app.route('/mysql')
+def mysql() -> 'html':
+    return render_template('mysql.html')
+@app.route('/flask')
+def flask() -> 'html':
+    return render_template('flask.html')
+@app.route('/sitepackages')
+def sitepackage() -> 'html':
+    return render_template('includeModuleToSitepackages.html')
+@app.route('/jinja2')
+def jinja2() -> 'html':
+    return render_template('jinja2.html', chapter = 'This sentence  is being rendered from chapters.py.Look in the code.')
+@app.route('/commandline')
+def commandline() -> 'html':
+    return render_template('commandlineSyntax.html')
+@app.route('/csv')
+def csv() -> 'html':
+    return render_template('csv.html')
